2a500f2f3c98436997439357ddfac81bb21a64e6
Suzy <69157347+SuzannaLin@users.noreply.github.com>
2020-11-10 10:58:47 +0100

    move softmax_helper to losses.py

--- utils/losses.py
+++ utils/losses.py
@@ -30,6 +30,15 @@ class consistency_weight(object):
 def CE_loss(input_logits, target_targets, ignore_index, temperature=1):
     return F.cross_entropy(input_logits/temperature, target_targets, ignore_index=ignore_index)
 
+# for FocalLoss
+def softmax_helper(x):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/nd_softmax.py
+    rpt = [1 for _ in range(len(x.size()))]
+    rpt[1] = x.size(1)
+    x_max = x.max(1, keepdim=True)[0].repeat(*rpt)
+    e_x = torch.exp(x - x_max)
+    return e_x / e_x.sum(1, keepdim=True).repeat(*rpt)
+
 def get_alpha(supervised_loader):
     # get number of classes
     num_labels = 0
