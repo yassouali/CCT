17df50bc57c63281419d35283ce9753f0de582fb
John Robert <trojrobert@gmail.com>
2020-12-29 14:50:50 -0500

    Update decoders.py

--- models/decoders.py
+++ models/decoders.py
@@ -82,7 +82,7 @@ class FeatureDropDecoder(nn.Module):
     def feature_dropout(self, x):
         attention = torch.mean(x, dim=1, keepdim=True)
         max_val, _ = torch.max(attention.view(x.size(0), -1), dim=1, keepdim=True)
-        threshold = max_val * np.random.uniform(0.7, 0.9)
+        threshold = max_val * np.random.uniform(0.6, 0.9)
         threshold = threshold.view(x.size(0), 1, 1, 1).expand_as(attention)
         drop_mask = (attention < threshold).float()
         return x.mul(drop_mask)
